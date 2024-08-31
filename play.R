584ef8668046c8fdb6d33a8a43f49ffdc16437c5
Don Boyd <donboyd5@gmail.com>
2024-08-31 06:37:31 -0400

    update play

--- play.R
+++ play.R
@@ -2,8 +2,14 @@
 source("data-raw/pkgtools.R")
 
 library(penvir)
+usethis::use_roxygen_md()
 
 load_all()
+devtools::document()
+devtools::build() # not needed, creates a tarball
+devtools::install()
+devtools::check()
+
 
 data(package="penvir")
 
@@ -16,3 +22,7 @@ names(trs)
 
 frs$calculate_benefits
 frs$beneficiaries
+
+
+# useful usethis functions
+use_data() # save data to data/
