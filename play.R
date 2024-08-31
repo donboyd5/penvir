ee25145bf1f7227a4435d9ff2af8e02837817af4
Don Boyd <donboyd5@gmail.com>
2024-08-31 07:08:44 -0400

    create package data

--- play.R
+++ play.R
@@ -3,6 +3,8 @@ source("data-raw/pkgtools.R")
 
 library(penvir)
 usethis::use_roxygen_md()
+use_mit_license(copyright_holder = NULL)
+usethis::use_data_raw() # create data-raw/ and Rbuildignore it
 
 load_all()
 devtools::document()
@@ -24,5 +26,19 @@ frs$calculate_benefits
 frs$beneficiaries
 
 
+# put environment data into inst/extdata
+usethis::use_directory("inst/extdata") # create the inst/extdata directory
+
+# create path to data if it exists (needs penvir installed)
+fpath <- system.file("extdata", "my_data.csv", package = "penvir")
+
+# it's ok to create subfolders
+system.file("extdata", "subfolder", "datafile.rds", package = "yourpackage")
+
+
+
+
+
 # useful usethis functions
 use_data() # save data to data/
+usethis::use_directory("inst/extdata") # create a special folder for the pension data
